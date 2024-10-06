1b0db57c29feafbd8c00554bafbd12cd6b8e7a9f
Victor Roman <Romantech91>
2024-10-06 15:49:36 -0700

    user controller updated

---
+++ src/controllers/userController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from 'express';
+import User from '../models/User.js';
+
+export const getAllUsers = async (req: Request, res: Response) => {
+  try {
+    const users = await User.find({});
+    res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: 'Unable to get users.', error: error });
+    }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findById(req.params.id).populate('thoughts').populate('friends');
+        if (!user) {
+           return res.status(404).json({ message: 'User not found.' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ message: 'Unable to get user.', error: error });
+    }
+};
+
+export const createUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (error) {
+        res.status(400).json({ message: 'Unable to create user.', error: error });
+    }
+};
+
+export const updateUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, 
+            { new: true, runValidators: true });
+        if (!user) {
+            return res.status(404).json({ message: 'User not found.' });
+        }
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(400).json({ message: 'Unable to update user.', error: error });
+    }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found'});
+        }
+        res.status(200).json({ message: 'User deleted.' });
+    } catch (error) {
+        res.status(400).json({ message: 'Unable to delete user.', error: error });
+    }
+};
---
+++ src/models/Thought.ts
@@ -0,0 +1,66 @@
+import { Schema, model, Types} from 'mongoose';
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp: Date) => timestamp.toISOString()
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+const ThoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp: Date) => timestamp.toISOString()
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    reactions: [reactionSchema]
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+  }
+);
+
+ThoughtSchema.virtual('reactionCount').get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model('Thought', ThoughtSchema);
+
+export default Thought;
