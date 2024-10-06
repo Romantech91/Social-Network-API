516e0a1c9f8125624995dfa6b7200e79f0c11217
Victor Roman <Romantech91>
2024-10-06 16:55:00 -0700

    user routes added in index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,10 @@
 import { Router } from 'express';
 import apiRoutes from './api/index.js';
+import userRoutes from './api/userRoutes.js';
+
 const router = Router();
 
 router.use('/api', apiRoutes);
+router.use('/api/users', userRoutes);
 
 export default router;
